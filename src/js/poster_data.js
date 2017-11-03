cbe8f18289efdd400f8f3b6278e7ea0f6d1d2c69
정민규 <eprkswu@sbs.co.kr>
2017-11-03 21:19:08 +0900

    페이지 개발 진행

---
+++ src/js/poster_data.js
@@ -0,0 +1,86 @@
+var iconBase = '../img/map_pin/';
+var posterBase = '../img/poster_image/';
+var icons = {
+    '1': iconBase+'pin_1.png',
+    '2': iconBase+'pin_2.png',
+    '3': iconBase+'pin_3.png'
+};
+var displays = [
+    {
+        title:'아름다운 반복',
+        writer:'김병호-이기준',
+        date:'날짜 협의중',
+        placename:'디자인하우스',
+        address:'서울 중구 동호로 310',
+        poster:posterBase+'1_replication.jpg',
+        icon:icons['1'],
+        position:[37.5619921,127.0030534],
+        marker:null
+    },
+    {
+        title:'보통의 정의',
+        writer:'박혜수-이동영',
+        date:'11.1 ~ 30',
+        placename:'연세필정신건강의학과의원 광화문점',
+        address:'서울 종로구 종로 19 르메이에르 종로타운 4층, 413-2, 414-1호',
+        poster:posterBase+'2_botong.jpg',
+        icon:icons['1'],
+        position:[37.5707885,126.9776416],
+        marker:null
+    },
+    {
+        title:'철학자들',
+        writer:'박제성-초코딩',
+        date:'11.1 ~ 30',
+        placename:'루프트 합정',
+        address:'서울 마포구 독막로8길 16',
+        poster:posterBase+'3_philosopher.jpg',
+        icon:icons['1'],
+        position:[37.5472225,126.9155797],
+        marker:null
+    },
+    {
+        title:'5개의 축',
+        writer:'백현주-정진열',
+        date:'11.1 ~ 30',
+        placename:'게방식당',
+        address:'서울 강남구 선릉로131길 17',
+        poster:posterBase+'4_axis.jpg',
+        icon:icons['1'],
+        position:[37.5172825,127.0372336],
+        marker:null
+    },
+    {
+        title:'헬리오스 벽지',
+        writer:'이윤성-신신',
+        date:'2017.11.7 ~ 2018.1.7',
+        placename:'더북소사이어티(에이랜드)',
+        address:'서울 강남구 논현로175길 78, 4층',
+        poster:posterBase+'5_hellios.jpg',
+        icon:icons['1'],
+        position:[37.5236768,127.0204874],
+        marker:null
+    },
+    {
+        title:'여기_거기',
+        writer:'이은경-조형석',
+        date:'11.1 ~ 30',
+        placename:'식물학',
+        address:'서울 강남구 신사동 516-16, 1층',
+        poster:posterBase+'6_here_there.jpg',
+        icon:icons['1'],
+        position:[37.5183871,127.0194376],
+        marker:null
+    },
+    {
+        title:'무제',
+        writer:'정소영-섬광',
+        date:'11.1 ~ 30',
+        placename:'문화공간 숨도-미소서식지',
+        address:'서울 마포구 백범로71 숨도빌딩 2층',
+        poster:posterBase+'7_untitled.jpg',
+        icon:icons['1'],
+        position:[37.5489621,126.9375352],
+        marker:null
+    }
+];
